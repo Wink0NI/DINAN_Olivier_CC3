fff60bed0705f8abf5ff25dd603addbe0fe1c1fd
Wink0NI <olivier2003nc@gmail.com>
2023-09-27 22:04:18 +1100

    random-nodejs

--- server-http.mjs
+++ server-http.mjs
@@ -6,23 +6,44 @@ const port = 8000;
 
 async function requestListener(request, response) {
     response.setHeader("Content-Type", "text/html");
+    const urlParts = request.url.split("/");
+    const basePath = urlParts[1];
+
     try {
-        const contents = await fs.readFile("index.html", "utf8");
-        switch (request.url) {
-            case "/index.html":
+        switch (basePath) {
+            case "":
+            case "index.html": {
+                const contents = await fs.readFile("index.html", "utf8");
                 response.writeHead(200);
-                return response.end(contents);
-            case "/random.html":
-                response.writeHead(200);
-                return response.end(`<html><p>${Math.floor(100 * Math.random())}</p></html>`);
-            default:
+                response.end(contents);
+                break;
+            }
+            case "random.html": {
+                const nb = Number.parseInt(urlParts[2]); // Extract the number from the URL
+
+                if (Number.isNaN(nb)) {
+                    response.writeHead(400); // Bad Request
+                    response.end("<html><p>400: BAD REQUEST</p></html>");
+                } else {
+                    let nb_random = "";
+                    for (let index = 0; index < nb; index++) {
+                        nb_random += `<p>${Math.floor(100 * Math.random())}</p>`;
+                    }
+                    response.writeHead(200);
+                    response.end(`<html>${nb_random}</html>`);
+                }
+                break;
+            }
+            default: {
                 response.writeHead(404);
-                return response.end(`<html><p>404: NOT FOUND</p></html>`);
+                response.end("<html><p>404: NOT FOUND</p></html>");
+                break;
+            }
         }
     } catch (error) {
         console.error(error);
         response.writeHead(500);
-        return response.end(`<html><p>500: INTERNAL SERVER ERROR</p></html>`);
+        response.end("<html><p>500: INTERNAL SERVER ERROR</p></html>");
     }
 }
 
